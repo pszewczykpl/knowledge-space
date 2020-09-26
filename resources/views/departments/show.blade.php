b3fb053007f60f239ef69d09d2c6e5f8831170e4
pszewczykpl <piotr.szewczyk96@gmail.com>
2020-09-27 00:59:03 +0200

    Added Create, Edit and Show View to Department Model

---
+++ resources/views/departments/show.blade.php
@@ -0,0 +1,127 @@
+@extends('master')
+
+@section('subheader')
+<div class="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+	<div class="d-flex align-items-center flex-wrap mr-2">
+		<h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5">{{ $title }}</h5>
+		<div class="subheader-separator subheader-separator-ver mt-2 mb-2 mr-4 bg-gray-200"></div>
+	</div>
+	<div class="d-flex align-items-center">
+		<a href="{{ url()->previous() }}" class="btn btn-md btn-clean btn-shadow font-weight-bold ml-1">
+			<span class="svg-icon navi-icon">
+				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+					<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+						<polygon points="0 0 24 0 24 24 0 24"/>
+						<rect fill="#000000" opacity="0.3" transform="translate(12.000000, 12.000000) scale(-1, 1) rotate(-90.000000) translate(-12.000000, -12.000000) " x="11" y="5" width="2" height="14" rx="1"/>
+						<path d="M3.7071045,15.7071045 C3.3165802,16.0976288 2.68341522,16.0976288 2.29289093,15.7071045 C1.90236664,15.3165802 1.90236664,14.6834152 2.29289093,14.2928909 L8.29289093,8.29289093 C8.67146987,7.914312 9.28105631,7.90106637 9.67572234,8.26284357 L15.6757223,13.7628436 C16.0828413,14.136036 16.1103443,14.7686034 15.7371519,15.1757223 C15.3639594,15.5828413 14.7313921,15.6103443 14.3242731,15.2371519 L9.03007346,10.3841355 L3.7071045,15.7071045 Z" fill="#000000" fill-rule="nonzero" transform="translate(9.000001, 11.999997) scale(-1, -1) rotate(90.000000) translate(-9.000001, -11.999997) "/>
+					</g>
+				</svg>
+			</span>
+			Anuluj
+		</a>
+		@can('update', $department)
+			<a href="{{ route('departments.edit', $department->id) }}" class="btn btn-md btn-light-primary btn-shadow font-weight-bold ml-1">
+				<span class="svg-icon navi-icon">
+					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+						<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+							<rect x="0" y="0" width="24" height="24"/>
+							<path d="M8,17.9148182 L8,5.96685884 C8,5.56391781 8.16211443,5.17792052 8.44982609,4.89581508 L10.965708,2.42895648 C11.5426798,1.86322723 12.4640974,1.85620921 13.0496196,2.41308426 L15.5337377,4.77566479 C15.8314604,5.0588212 16,5.45170806 16,5.86258077 L16,17.9148182 C16,18.7432453 15.3284271,19.4148182 14.5,19.4148182 L9.5,19.4148182 C8.67157288,19.4148182 8,18.7432453 8,17.9148182 Z" fill="#000000" fill-rule="nonzero" transform="translate(12.000000, 10.707409) rotate(-135.000000) translate(-12.000000, -10.707409) "/>
+							<rect fill="#000000" opacity="0.3" x="5" y="20" width="15" height="2" rx="1"/>
+						</g>
+					</svg>
+				</span>
+				Edytuj
+			</a>
+		@endcan
+		@can('delete', $department)
+			<a onclick='document.getElementById("departments_destroy_{{ $department->id }}").submit();' class="btn btn-md btn-light-danger btn-shadow font-weight-bold ml-1">
+				<span class="svg-icon navi-icon">
+					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+						<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+							<rect x="0" y="0" width="24" height="24"/>
+							<path d="M6,8 L18,8 L17.106535,19.6150447 C17.04642,20.3965405 16.3947578,21 15.6109533,21 L8.38904671,21 C7.60524225,21 6.95358004,20.3965405 6.89346498,19.6150447 L6,8 Z M8,10 L8.45438229,14.0894406 L15.5517885,14.0339036 L16,10 L8,10 Z" fill="#000000" fill-rule="nonzero"/>
+							<path d="M14,4.5 L14,3.5 C14,3.22385763 13.7761424,3 13.5,3 L10.5,3 C10.2238576,3 10,3.22385763 10,3.5 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" fill="#000000" opacity="0.3"/>
+						</g>
+					</svg>
+				</span>
+				Usuń
+			</a>
+			{{ Form::open([ 'method'  => 'delete', 'route' => [ 'departments.destroy', $department->id ], 'id' => 'departments_destroy_' . $department->id ]) }}{{ Form::close() }}
+		@endcan
+	</div>
+</div>
+@stop
+
+@section('content')
+<div class="container">
+    @if(count($errors) > 0)
+    <div class="alert alert-custom alert-notice alert-light-danger fade show" role="alert">
+        <div class="alert-icon"><i class="flaticon-warning"></i></div>
+        <div class="alert-text">
+            @foreach($errors->all() as $error)
+                {{ $error }} <br>
+            @endforeach
+        </div>
+    </div>
+    @endif
+    <div class="card card-custom gutter-bs">
+        <div class="card-header card-header-tabs-line">
+            <div class="card-toolbar">
+                <ul class="nav nav-tabs nav-tabs-space-lg nav-tabs-line nav-tabs-bold nav-tabs-line-3x" role="tablist">
+                    <li class="nav-item mr-3">
+                        <a class="nav-link active" data-toggle="tab" href="#info" role="tab">
+                            <span class="nav-icon mr-2">
+                                <span class="svg-icon mr-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <rect x="0" y="0" width="24" height="24"/>
+                                            <path d="M5.5,4 L18.5,4 C19.3284271,4 20,4.67157288 20,5.5 L20,9.5 C20,10.3284271 19.3284271,11 18.5,11 L5.5,11 C4.67157288,11 4,10.3284271 4,9.5 L4,5.5 C4,4.67157288 4.67157288,4 5.5,4 Z M11,6 C10.4477153,6 10,6.44771525 10,7 C10,7.55228475 10.4477153,8 11,8 L13,8 C13.5522847,8 14,7.55228475 14,7 C14,6.44771525 13.5522847,6 13,6 L11,6 Z" fill="#000000" opacity="0.3"/>
+                                            <path d="M5.5,13 L18.5,13 C19.3284271,13 20,13.6715729 20,14.5 L20,18.5 C20,19.3284271 19.3284271,20 18.5,20 L5.5,20 C4.67157288,20 4,19.3284271 4,18.5 L4,14.5 C4,13.6715729 4.67157288,13 5.5,13 Z M11,15 C10.4477153,15 10,15.4477153 10,16 C10,16.5522847 10.4477153,17 11,17 L13,17 C13.5522847,17 14,16.5522847 14,16 C14,15.4477153 13.5522847,15 13,15 L11,15 Z" fill="#000000"/>
+                                        </g>
+                                    </svg>
+                                </span>
+                            </span>
+                            <span class="nav-text font-weight-bold">Szczegóły</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="card-body px-0">
+            <div class="tab-content">
+                <div class="tab-pane active px-7" id="info" role="tabpanel">
+                    <div class="row">
+                        <div class="col-xl-2"></div>
+                        <div class="col-xl-7 my-2">
+                            <div class="row">
+                                <label class="col-3"></label>
+                                <div class="col-9">
+                                    <h6 class="text-dark font-weight-bold mb-10">Dane departamentu:</h6>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-form-label col-3 text-lg-right text-left">Nazwa:</label>
+                                <div class="col-9">
+                                    <input class="form-control form-control-lg form-control-solid" type="text" name="name" id="name" value="{{ $department->name }}" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-form-label col-3 text-lg-right text-left">Kod:</label>
+                                <div class="col-9">
+                                    <input class="form-control form-control-lg form-control-solid" type="text" name="code" id="code" value="{{ $department->code }}" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-form-label col-3 text-lg-right text-left">Opis departamentu:</label>
+                                <div class="col-9">
+                                    <textarea class="form-control form-control-lg form-control-solid" rows="4" name="description" id="description" disabled>{{ $department->description }}</textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
