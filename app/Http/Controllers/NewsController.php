ad4c6f2ebeedb79b3e9c0915458cafca3f9e0ebc
pszewczykpl <piotr.szewczyk96@gmail.com>
2020-09-26 23:52:32 +0200

    Corrected wrong middlewares in _construct method Controllers

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -19,7 +19,7 @@ class NewsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except(['index', 'show']);
+        $this->middleware('auth');
     }
     
     /**
