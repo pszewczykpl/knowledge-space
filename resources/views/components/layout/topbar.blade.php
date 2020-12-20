6beb8565e5f55a322dd1a17a1c080bef57ff791b
pszewczykpl <piotr.szewczyk96@gmail.com>
2020-12-20 03:58:14 +0100

    Updated admin menu

--- resources/views/components/layout/topbar.blade.php
+++ resources/views/components/layout/topbar.blade.php
@@ -45,22 +45,6 @@
 									</a>
 								</li>
 								@endcan
-								@can('viewany', App\Models\FileCategory::class)
-								<li class="menu-item {{ (request()->routeIs('file-categories.*')) ? 'menu-item-active' : '' }}" aria-haspopup="true">
-									<a href="{{ route('file-categories.index') }}" class="menu-link">
-										<span class="svg-icon menu-icon">
-											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-												<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-													<rect x="0" y="0" width="24" height="24"/>
-													<path d="M10,4 L21,4 C21.5522847,4 22,4.44771525 22,5 L22,7 C22,7.55228475 21.5522847,8 21,8 L10,8 C9.44771525,8 9,7.55228475 9,7 L9,5 C9,4.44771525 9.44771525,4 10,4 Z M10,10 L21,10 C21.5522847,10 22,10.4477153 22,11 L22,13 C22,13.5522847 21.5522847,14 21,14 L10,14 C9.44771525,14 9,13.5522847 9,13 L9,11 C9,10.4477153 9.44771525,10 10,10 Z M10,16 L21,16 C21.5522847,16 22,16.4477153 22,17 L22,19 C22,19.5522847 21.5522847,20 21,20 L10,20 C9.44771525,20 9,19.5522847 9,19 L9,17 C9,16.4477153 9.44771525,16 10,16 Z" fill="#000000"/>
-													<rect fill="#000000" opacity="0.3" x="2" y="4" width="5" height="16" rx="1"/>
-												</g>
-											</svg>
-										</span>
-										<span class="menu-text">Kategorie dokumentów</span>
-									</a>
-								</li>
-								@endcan
 								@can('viewany', App\Models\PostCategory::class)
 								<li class="menu-item {{ (request()->routeIs('post-categories.*')) ? 'menu-item-active' : '' }}" aria-haspopup="true">
 									<a href="{{ route('post-categories.index') }}" class="menu-link">
@@ -77,9 +61,8 @@
 									</a>
 								</li>
 								@endcan
-								@can('viewany', App\Models\File::class)
-								<li class="menu-item {{ (request()->routeIs('files.*')) ? 'menu-item-active' : '' }}" aria-haspopup="true">
-									<a href="{{ route('files.index') }}" class="menu-link">
+								<li class="menu-item menu-item-submenu {{ ((request()->routeIs('files.*')) or (request()->routeIs('file-categories.*'))) ? 'menu-item-active' : '' }}" data-menu-toggle="hover" aria-haspopup="true">
+									<a href="javascript:;" class="menu-link menu-toggle">
 										<span class="svg-icon menu-icon">
 											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
 												<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -91,9 +74,53 @@
 											</svg>
 										</span>
 										<span class="menu-text">Dokumenty</span>
+										<i class="menu-arrow"></i>
 									</a>
+									<div class="menu-submenu menu-submenu-classic menu-submenu-right">
+										<ul class="menu-subnav">
+											@can('viewany', App\Models\File::class)
+											<li class="menu-item {{ (request()->routeIs('files.index')) ? 'menu-item-active' : '' }}" aria-haspopup="true">
+												<a href="{{ route('files.index') }}" class="menu-link">
+													<i class="menu-bullet menu-bullet-dot">
+														<span></span>
+													</i>
+													<span class="menu-text">Przeglądaj listę dokumentów</span>
+												</a>
+											</li>
+											@endcan
+											@can('create', App\Models\File::class)
+											<li class="menu-item {{ (request()->routeIs('files.create')) ? 'menu-item-active' : '' }}" aria-haspopup="true">
+												<a href="{{ route('files.create') }}" class="menu-link">
+													<i class="menu-bullet menu-bullet-dot">
+														<span></span>
+													</i>
+													<span class="menu-text">Dodaj nowy dokument</span>
+												</a>
+											</li>
+											@endcan
+											@can('viewany', App\Models\FileCategory::class)
+											<li class="menu-item {{ (request()->routeIs('file-categories.index')) ? 'menu-item-active' : '' }}" aria-haspopup="true">
+												<a href="{{ route('file-categories.index') }}" class="menu-link">
+													<i class="menu-bullet menu-bullet-dot">
+														<span></span>
+													</i>
+													<span class="menu-text">Przeglądaj listę kategorii dokumentów</span>
+												</a>
+											</li>
+											@endcan
+											@can('viewany', App\Models\FileCategory::class)
+											<li class="menu-item {{ (request()->routeIs('file-categories.create')) ? 'menu-item-active' : '' }}" aria-haspopup="true">
+												<a href="{{ route('file-categories.create') }}" class="menu-link">
+													<i class="menu-bullet menu-bullet-dot">
+														<span></span>
+													</i>
+													<span class="menu-text">Dodaj nową kategorię dokumentów</span>
+												</a>
+											</li>
+											@endcan
+										</ul>
+									</div>
 								</li>
-								@endcan
 							</ul>
 						</div>
 					</li>
