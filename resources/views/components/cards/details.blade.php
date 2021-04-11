45392d7f41d35bcdd01a4aab357cb5e76bb774aa
pszewczykpl <pszewczykpl@gmail.com>
2021-04-12 01:10:15 +0200

    Updated detaild card view

--- resources/views/components/cards/details.blade.php
+++ resources/views/components/cards/details.blade.php
@@ -1,9 +1,9 @@
 <div class="card card-custom gutter-b">
-	<div class="card-header h-auto py-sm-0 mb-3">
-		<div class="card-title">
-			<h3 class="card-label">{{ $title }}
-			<span class="d-block text-muted pt-2 font-size-sm">{{ $description }}</span></h3>
-		</div>
+	<div class="card-header align-items-center border-0 mt-4">
+		<h3 class="card-title align-items-start flex-column">
+			<span class="font-weight-bolder text-dark">{{ $title }}</span>
+			<span class="text-muted mt-3 font-weight-bold font-size-sm">{{ $description }}</span>
+		</h3>
 	</div>
 	{{ $slot }}
 	<div class="mt-3"></div>
