deb80e3205d8fdc0b28f0de2888dca161bf7cc22
pszewczykpl <pszewczykpl@gmail.com>
2021-04-11 02:15:10 +0200

    Exported javascript to external files in views

---
+++ resources/views/components/cards/help-us.blade.php
@@ -0,0 +1,19 @@
+<div class="card card-custom gutter-b bg-diagonal bg-diagonal-light-primary mt-10">
+    <div class="card-body">
+        <div class="d-flex align-items-center justify-content-between p-4 flex-lg-wrap flex-xl-nowrap">
+            <div class="d-flex flex-column mr-5">
+							<span class="h4 font-weight-bold text-dark">
+								Brakuje czegoś?
+							</span>
+                <p class="text-dark-50 h6">
+                    Pomóż nam ulepszać system i zgłoś to!
+                </p>
+            </div>
+            <div class="ml-6 ml-lg-0 ml-xxl-6 flex-shrink-0">
+                <a href="mailto:piotr.szewczyk@openlife.pl" target="_blank" class="btn font-weight-bolder text-uppercase btn-primary py-4 px-6">
+                    Zgłoś to!
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/trash/systems.blade.php
+++ resources/views/trash/systems.blade.php
@@ -60,37 +60,5 @@
 @stop
 
 @push('scripts')
-{{-- <script src="{{ asset('js/pages/trash/systems.js') }}" type="text/javascript"></script> --}}
-<script>
-	$(document).ready(function() {
-		$("#table").DataTable( {
-			responsive: true,
-			processing: true,
-			dom: "<'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>",
-			lengthMenu: [5, 15, 25, 50],
-			pageLength: 15,
-			language: {
-				"decimal":        "",
-				"emptyTable":     "Brak danych do wyświetlenia",
-				"info":           "Wyświetlono _START_ - _END_ z _TOTAL_ rekordów",
-				"infoEmpty":      "Wyświetlono 0 z 0 rekordów",
-				"infoFiltered":   "(wyszukano z _MAX_ rekordów)",
-				"infoPostFix":    "",
-				"thousands":      ",",
-				"lengthMenu":     "Pokaż _MENU_ rekordów",
-				"loadingRecords": "Ładowanie...",
-				"processing":     "Procesowanie...",
-				"search":         "Szukaj:",
-				"zeroRecords":    "Brak pasujących rekordów",
-				"paginate": {
-					"first":      "<<",
-					"last":       ">>",
-					"next":       ">",
-					"previous":   "<"
-				}
-			},
-			"order": [2, "desc"]
-		});
-	});
-</script>
+ <script src="{{ asset('js/pages/trash/systems.js') }}" type="text/javascript"></script>
 @endpush
