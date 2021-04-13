2e392e5f6d6644847df190cb82fbf450bad9de8f
pszewczykpl <pszewczykpl@gmail.com>
2021-04-14 00:51:43 +0200

    App version deleted from .env file (now only in config/app.php file)

--- config/app.php
+++ config/app.php
@@ -25,7 +25,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    'version' => env("APP_VERSION", "0.5"),
+    'version' => '0.5',
 
     /*
     |--------------------------------------------------------------------------
