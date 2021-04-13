c9c9f9078be9b4cfe3df3a4f619db5c25720bfbf
pszewczykpl <pszewczykpl@gmail.com>
2021-04-14 00:23:05 +0200

    Updated search form

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -44,101 +44,86 @@ class SearchController extends Controller
     {
         $value = trim($request->value);
         $active = $request->non_active ? ['A', 'N'] : ['A'];
-
-        // MAYBE TO DO: departments, users, file categories, files, notes, replies, permissions, post categories, attachments
-        /**
-         * Return Systems
-         * TODO: search by:  name url description
-         */
-        // $systems = \App\Models\Protective
-
-        /**
-         * Return Partners
-         * TODO: search by  name number_rau code nip regon type
-         */
-        // $partners = \App\Models\Protective
-
-        /**
-         * Return News
-         * TODO: search by  content
-         */
-        // $news = \App\Models\Protective
+        $columns = [
+            'investments' => ['name', 'code_owu', 'code_toil', 'code', 'group', 'dist'],
+            'protectives' => ['name', 'code_owu', 'code', 'dist'],
+            'bancassurances' => ['name', 'code_owu', 'code', 'dist'],
+            'employees' => ['name', 'code_owu'],
+            'funds' => ['name', 'code', 'type'],
+            'risks' => ['name', 'code', 'type'],
+            'posts' => ['title', 'content'],
+            'news' => ['content'],
+            'partners' => ['name', 'nip', 'regon', 'numer_rau'],
+            'systems' => ['name', 'url'],
+            'files' => ['name'],
+            'departments' => ['name'],
+        ];
 
         /**
-         * Return Posts
-         * TODO: search by  title content
-         */
-        // $posts = \App\Models\Protective
-
-        /**
-         * Return Risks
-         * TODO: search by  code name category group grace_period
-         */
-        // $risks = \App\Models\Protective
-
-        /**
-         * Return Funds
-         * TODO: search by  code name type currency status start_date
-         */
-        // $funds = \App\Models\Protective
-
-        /**
-         * Return Employees
-         * TODO: search by  name code_owu edit_date status
+         * Search Employees.
          */
         $employees = \App\Models\Employee::select('*')
-            ->where('name', 'like', '%' . $value . '%')
-            ->orWhere('code_owu', 'like', '%' . $value . '%')
-            ->take(25)
-            ->get();
-
-        /**
-         * Return Bancassurances
-         * TODO: search by  name code dist_short dist code_owu subscription edit_date status
-         */
-        $bancassurances = \App\Models\Bancassurance::select('*')
-            ->where('name', 'like', '%' . $value . '%')
-            ->orWhere('code', 'like', '%' . $value . '%')
-            ->orWhere('code_owu', 'like', '%' . $value . '%')
-            ->take(25)
-            ->get();
-
-        /**
-         * Return Protectives
-         * search by  name code dist_short dist code_owu subscription edit_date status
-         */
-        $protectives = \App\Models\Protective::select('*')
-            ->where('name', 'like', '%' . $value . '%')
-            ->orWhere('code', 'like', '%' . $value . '%')
-            ->orWhere('code_owu', 'like', '%' . $value . '%')
-            ->take(25)
-            ->get();
-
-        /**
-         * Redirect to Investment when only one Investment find.
-         */
-        foreach(['name', 'code_owu', 'code_toil', 'code'] as $column) {
-            $investment = \App\Models\Investment::select('id')->where($column, $value)->whereIn('status', $active)->get();
-            if($investment->count() === 1) {
-                return redirect()->route('investments.show', $investment->first()->id);
-            }
-        }
-        /**
-         * Search Investments when more then one Investments find.
-         */
-        $investments = \App\Models\Investment::select('*')
-            ->where(function ($query) use ($value) {
-                foreach(['name', 'code_owu', 'code_toil', 'code'] as $column) {
+            ->where(function ($query) use ($value, $columns) {
+                foreach($columns['employees'] as $column) {
                     $query->orWhere($column, 'like', '%' . $value . '%');
                 }
             })
             ->whereIn('status', $active)
-            ->take(25)
             ->get();
+
+        /**
+         * Search Bancassurances.
+         */
+        $bancassurances = \App\Models\Bancassurance::select('*')
+            ->where(function ($query) use ($value, $columns) {
+                foreach($columns['bancassurances'] as $column) {
+                    $query->orWhere($column, 'like', '%' . $value . '%');
+                }
+            })
+            ->whereIn('status', $active)
+            ->get();
+
+        /**
+         * Search Protectives.
+         */
+        $protectives = \App\Models\Protective::select('*')
+            ->where(function ($query) use ($value, $columns) {
+                foreach($columns['protectives'] as $column) {
+                    $query->orWhere($column, 'like', '%' . $value . '%');
+                }
+            })
+            ->whereIn('status', $active)
+            ->get();
+
+        /**
+         * Search Investments.
+         */
+        $investments = \App\Models\Investment::select('*')
+            ->where(function ($query) use ($value, $columns) {
+                foreach($columns['investments'] as $column) {
+                    $query->orWhere($column, 'like', '%' . $value . '%');
+                }
+            })
+            ->whereIn('status', $active)
+            ->get();
+
+        if($investments->count() === 1 and (count($protectives) === 0 and count($bancassurances) === 0 and count($employees) === 0)) {
+            return redirect()->route('investments.show', $investments->first()->id);
+        }
+        if($protectives->count() === 1 and (count($investments) === 0 and count($bancassurances) === 0 and count($employees) === 0)) {
+            return redirect()->route('protectives.show', $protectives->first()->id);
+        }
+        if($bancassurances->count() === 1 and (count($protectives) === 0 and count($investments) === 0 and count($employees) === 0)) {
+            return redirect()->route('bancassurances.show', $bancassurances->first()->id);
+        }
+        if($employees->count() === 1 and (count($protectives) === 0 and count($bancassurances) === 0 and count($investments) === 0)) {
+            return redirect()->route('employees.show', $employees->first()->id);
+        }         
 
         return view('search.results', [
             'title' => 'Wyniki wyszukiwania',
             'value' => $value,
+            'active' => $active,
             'investments' => $investments,
             'protectives' => $protectives,
             'bancassurances' => $bancassurances,
