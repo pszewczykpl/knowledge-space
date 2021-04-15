6c35ebcb8d2687ba4d03e23db162d74b1606fe7a
pszewczykpl <pszewczykpl@gmail.com>
2021-04-15 02:19:14 +0200

    Updated deployment.sh file

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,9 @@
+php artisan down --render="errors::maintenance" --secret="deployment"
 git fetch --all
 git reset --hard origin/master
 php composer.phar update --optimize-autoloader --no-dev
 php artisan app:refresh
+php artisan up
 php artisan app:version
 echo "Click enter to close..."
 read
