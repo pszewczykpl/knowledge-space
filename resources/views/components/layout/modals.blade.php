27c16e349f6464edf13e29af19d7e4b1be51726d
pszewczykpl <pszewczykpl@gmail.com>
2021-08-06 19:43:27 +0200

    Minified JS and CSS files

--- resources/views/components/layout/modals.blade.php
+++ resources/views/components/layout/modals.blade.php
@@ -1,6 +1,6 @@
 <div class="modal" id="policy_calculator_modal" tabindex="-1" role="dialog" aria-labelledby="PolicyCalculatorModal" aria-hidden="true">
 	<div class="modal-dialog" role="document">
-		<div class="modal-content">
+		<div class="modal-content card-shadow card-rounded">
 			<div class="modal-header">
 				<h5 class="modal-title" id="PolicyCalculatorModal">
 					Kalkulator miesięcy polisowych
@@ -16,38 +16,27 @@
 					</label>
 					<div class="col-lg-9 col-md-9 col-sm-12">
 						<div class="input-group date">
-							<input type="text" class="form-control m-input" placeholder="Wskaż datę" id="MyDate"/>
-							<div class="input-group-append">
-								<span class="input-group-text">
-									<i class="la la-calendar-check-o"></i>
-								</span>
-							</div>
+							<input type="text" class="form-control datepicker" placeholder="Wskaż datę" id="MyDate"/>
 						</div>
 					</div>
 				</div>
-				<div class="form-group m-form__group row">
+				<div class="form-group m-form__group row pt-3">
 					<label class="col-form-label col-lg-3 col-sm-12">
 						Koniec
 					</label>
 					<div class="col-lg-9 col-md-9 col-sm-12">
 						<div class="input-group date" >
-							<input type="text" class="form-control m-input" value="<?php echo date("Y-m-d"); ?>" id="MyDateEnd"/>
-							<div class="input-group-append">
-								<span class="input-group-text">
-									<i class="la la-calendar"></i>
-								</span>
-							</div>
+							<input type="text" class="form-control datepicker" value="<?php echo date("Y-m-d"); ?>" id="MyDateEnd"/>
 						</div>
 					</div>
 				</div>
-    			<div style="padding-top: 10px; font-size: 15px" class="alert alert-light" role="alert">
-    			    Rok polisowy: <span id="rok"></span><br/>
-    			    Miesiąc polisowy: <span id="miesiac"></span><br/>
+    			<div style="padding-top: 10px; font-size: 15px">
+					<div class="pt-3">Rok polisowy: <span id="rok"></span><br/></div>
+					<div class="pt-3">Miesiąc polisowy: <span id="miesiac"></span><br/></div>
     			</div>
     		</div>
 			<div class="modal-footer" style="border: 0px; padding-top: 0px;">
-				<button type="button" class="btn btn-text-dark btn-hover-light-dark mr-2" data-dismiss="modal">Wyjdź</button>
-				<button type="button" class="btn btn-primary btn-shadow font-weight-bold mr-2" onclick="policyCalcFunc()">Oblicz</button>
+				<button type="button" class="btn btn-primary btn-shadow font-weight-bold me-2" onclick="policyCalcFunc()">Oblicz</button>
 			</div>
 		</div>
 	</div>
