cad50f73ef2cba221d27f21bd3c806372f551b08
pszewczykpl <pszewczykpl@gmail.com>
2021-08-07 21:29:32 +0200

    Added comments

--- app/Traits/CacheModels.php
+++ app/Traits/CacheModels.php
@@ -15,15 +15,21 @@ trait CacheModels {
      */
     private function getCachedRelation(string $relation, array $tags = []): mixed
     {
+        // Check if relation is loaded now, return loaded data if true
         if ($this->relationLoaded($relation)) {
             return $this->getRelationValue($relation);
         }
 
+        // Add: custom tags from function arg and name of database tables: this and related
         array_push($tags, $this->getTable(), $this->{$relation}()->getRelated()->getTable());
-        $data = Cache::tags($tags)->rememberForever($this->getTable() . '_' . $this->id . '_' . $relation, function () use ($relation) {
-            return $this->getRelationValue($relation);
-        });
 
+        // Get relation data from database and add to cache
+        $data = Cache::tags($tags)
+            ->rememberForever($this->getTable() . '_' . $this->id . '_' . $relation, function () use ($relation) {
+                return $this->getRelationValue($relation);
+            });
+
+        // Load data into relation
         $this->setRelation($relation, $data);
 
         return $data;
