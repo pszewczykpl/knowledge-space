ce424a5cde45b7a1f202fbff8eb57118b58f5ed4
pszewczykpl <pszewczykpl@gmail.com>
2021-08-11 20:48:05 +0200

    Changed shell_exec to exec function

--- app/Http/Controllers/SystemPropertyController.php
+++ app/Http/Controllers/SystemPropertyController.php
@@ -51,6 +51,6 @@ class SystemPropertyController extends Controller
 
     public function getNewAppVersionFromGit(Request $request)
     {
-        echo shell_exec('sh ../deployment.sh');
+        echo exec('sh ../deployment.sh');
     }
 }
