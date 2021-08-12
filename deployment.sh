8de4daffb09c39b4a9faf4721a086a0ed7c415da
pszewczykpl <pszewczykpl@gmail.com>
2021-08-12 11:39:29 +0200

    Deleted unsused deployment.sh file

--- deployment.sh
+++
@@ -1,9 +0,0 @@
-php artisan down --render="errors::maintenance" --secret="deployment"
-git fetch --all
-git reset --hard origin/master
-php composer.phar update --optimize-autoloader --no-dev
-php artisan app:refresh
-php artisan up
-php artisan app:version
-echo "Click enter to close..."
-read
