e860475270ea53fa950b234d8be80f01f098cb40
pszewczykpl <pszewczykpl@gmail.com>
2021-08-15 22:03:00 +0200

    Incremented app version

--- config/app.php
+++ config/app.php
@@ -25,7 +25,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    'version' => '6.0.9',
+    'version' => '6.9',
 
     /*
     |--------------------------------------------------------------------------
