e3baaeaecadc1584f0bcba957c44901017381546
pszewczykpl <pszewczykpl@gmail.com>
2021-11-24 14:57:46 +0100

    Added storing view config in user profile

--- app/Http/Controllers/SystemConfigurationController.php
+++ app/Http/Controllers/SystemConfigurationController.php
@@ -5,13 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\SystemProperty;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
 
 class SystemConfigurationController extends Controller
 {
     public function switchDarkMode(Request $request)
     {
         $dark_mode = !session('dark_mode', config('view.default_dark_mode'));
+        
         session()->put('dark_mode', $dark_mode);
+        if(Auth::check()) {
+            Auth::user()->v_dark_mode = $dark_mode;
+            Auth::user()->save();
+        }
 
         return redirect()->back();
     }
--- app/Http/Middleware/SystemConfiguration.php
+++ app/Http/Middleware/SystemConfiguration.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Auth;
 
 class SystemConfiguration
 {
@@ -17,7 +18,10 @@ class SystemConfiguration
      */
     public function handle(Request $request, Closure $next)
     {
-        $dark_mode = session('dark_mode', config('view.default_dark_mode'));
+        if(Auth::check())
+            $dark_mode = Auth::user()->v_dark_mode;
+        else
+            $dark_mode = session('dark_mode', config('view.default_dark_mode'));
         View::share('dark_mode', $dark_mode);
 
         return $next($request);
