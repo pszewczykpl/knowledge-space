eee37f17c2e0585f97e71f1581ad9ef552470bfd
pszewczykpl <pszewczykpl@gmail.com>
2021-12-05 15:43:52 +0100

    Added background illustration in auth view

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
 
     <body id="kt_body" class="@if($dark_mode) dark-mode @else bg-body @endif">
         <div class="d-flex flex-column flex-root">
-            <div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed">
+            <div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed" style="background-image: url({{ asset('images/illustrations/background.png') }})">
                 <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
                     <a href="{{ route('login') }}" class="mb-12">
                         <img alt="Logo" src="{{ asset('/images/logos/main.png') }}" class="h-55px" />
