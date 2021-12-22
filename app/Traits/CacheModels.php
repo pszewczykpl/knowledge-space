4a1b892777b67ee3041b50b89627e3a0e49947e8
pszewczykpl <pszewczykpl@gmail.com>
2021-12-22 15:41:15 +0100

    Optimized getCachedRelation method

--- app/Traits/CacheModels.php
+++ app/Traits/CacheModels.php
@@ -10,22 +10,17 @@ trait CacheModels {
      * Get relations data from cache.
      *
      * @param string $relation
-     * @param array $tags
      * @return mixed
      */
-    private function getCachedRelation(string $relation, array $tags = []): mixed
+    private function getCachedRelation(string $relation): mixed
     {
         // Check if relation is loaded now, return loaded data if true
-        if ($this->relationLoaded($relation)) {
+        if ($this->relationLoaded($relation))
             return $this->getRelationValue($relation);
-        }
-
-        // Add: custom tags from function arg and name of database tables: this and related
-        array_push($tags, $this->getTable(), $this->{$relation}()->getRelated()->getTable());
 
         // Get relation data from database and add to cache
-        $data = Cache::tags($tags)
-            ->rememberForever($this->getTable() . ':' . $this->id . ':' . $relation, function () use ($relation) {
+        $data = Cache::tags([$this->getTable(), $this->{$relation}()->getRelated()->getTable()])
+            ->remember("{$this->getTable()}:$this->id:$relation", 60*60*12,function () use ($relation) {
                 return $this->getRelationValue($relation);
             });
 
