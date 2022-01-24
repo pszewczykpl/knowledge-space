a01a9a4401acd640873fede238e3e5f75bf31f02
pszewczykpl <pszewczykpl@gmail.com>
2022-01-24 12:17:08 +0100

    Incrementing app version

--- config/app.php
+++ config/app.php
@@ -25,7 +25,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    'version' => '8.4',
+    'version' => '8.5',
 
     /*
     |--------------------------------------------------------------------------
