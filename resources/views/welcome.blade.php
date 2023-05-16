36bab40e247b0fa51b2dccd6c8524c8d8969897b
Piotr Szewczyk <pszewczyk@digitalvirgo.com>
2023-05-16 14:53:54 +0200

    Stable HTTPS in assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
     <meta name="csrf-token" value="{{ csrf_token() }}" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700">
-    <link rel="stylesheet" href="{{ asset('/splash-screen.css') }}">
-    <link rel="icon" href="{{ asset('favicon.ico') }}" />
+    <link rel="stylesheet" href="https://bazawiedzy.int.openlife.pl/splash-screen.css">
+    <link rel="icon" href="https://bazawiedzy.int.openlife.pl/favicon.ico" />
     <title>Baza Wiedzy</title>
-    <script type="module" src="{{ asset('/assets/index.f718b679.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('/assets/index.735c10f3.css') }}">
+    <script type="module" src="https://bazawiedzy.int.openlife.pl/assets/index.f718b679.js"></script>
+    <link rel="stylesheet" href="https://bazawiedzy.int.openlife.pl/assets/index.735c10f3.css">
 </head>
 <body class="page-loading">
 <noscript>
