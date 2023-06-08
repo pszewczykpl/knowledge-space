e7a972553dd743f41eecd5ebd62830990137b99f
Piotr Szewczyk <pszewczyk@digitalvirgo.com>
2023-06-09 00:58:14 +0200

    Deleted new-database folder

--- new-database/user_roles.sql
+++
@@ -1,3 +0,0 @@
-insert into user_roles (id, user_id, role_id, created_at, updated_at)
-values  (1, 1, 1, '2023-05-14 15:44:28', '2023-05-14 15:44:28'),
-        (2, 23, 1, '2023-05-14 15:44:28', '2023-05-14 15:44:28');
